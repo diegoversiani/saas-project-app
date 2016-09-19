6f3930153e77e6ef020c9f1194f3898b684b670c
diegoversiani <diegoversiani@gmail.com>
2016-09-19 18:30:44 -0300

    Attempt to solve issue for db:migrate in heroku.

--- db/migrate/20160919194517_create_tenants_users_join_table.rb
+++ db/migrate/20160919194517_create_tenants_users_join_table.rb
@@ -1,7 +1,7 @@
 class CreateTenantsUsersJoinTable < ActiveRecord::Migration
   def change
     create_join_table :tenants, :users do |t|
-      t.index [:tenant_id, :user_id]
+      # t.index [:tenant_id, :user_id]
       # t.index [:user_id, :tenant_id]
     end
   end
